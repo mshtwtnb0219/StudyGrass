6abc7476e5a9c4393075694e0335fae8c3846eaa
mshtwtnb0219 <b77yellow@gmail.com>
2024-03-19 10:27:11 +0900

    ビルドに不要な設定項目を削除

--- studygrass2/vue.config.js
+++ studygrass2/vue.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from 'vite'
-
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  define: {
-    '__VUE_PROD_DEVTOOLS__': false,
-    '__VUE_OPTIONS_API__': true,
-    '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': true
-  }
-
+  transpileDependencies: true
+  
 })
