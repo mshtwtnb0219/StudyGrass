b6515ca8a24eeba7ca72dab49e3621c4d0ec1105
mshtwtnb0219 <b77yellow@gmail.com>
2024-03-24 22:25:38 +0900

    firebaseConfigを削除しておく

--- studygrass2/public/firebaseConfig.js
+++ studygrass2/public/firebaseConfig.js
@@ -1,13 +1,5 @@
 const firebaseConfig = {
 // ここにAPIkeyを貼り付ける
-apiKey: "AIzaSyBiUrDyot1oPSFOaWYomcYvaTee8uB4Pbo",
-authDomain: "studygrass2.firebaseapp.com",
-projectId: "studygrass2",
-storageBucket: "studygrass2.appspot.com",
-messagingSenderId: "339139170002",
-appId: "1:339139170002:web:eb87d8a40a1a77c60ee70b",
-measurementId: "G-1VBF2SH3G6"
-
 
   };
 
